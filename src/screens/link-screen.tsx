6020496efd5e10addcab4b5ec26f38258a3a8098
Finn Kiesinger <finnkiesinger@Finns-MBP.fritz.box>
2021-11-02 12:17:44 +0100

    commit

--- src/screens/link-screen.tsx
+++ src/screens/link-screen.tsx
@@ -2,6 +2,7 @@ import TextInput from '../components/text-input';
 import CenterForm from '../components/center-form';
 import Button from '../components/button';
 import styled from 'styled-components';
+import { useState, ChangeEvent } from 'react';
 
 const FormHeading = styled.div`
 	padding: 0.25rem;
@@ -12,20 +13,36 @@ const FlexEnd = styled.div`
 	justify-content: flex-end;
 `;
 
+const defaultConfig = {
+	apiKey: '',
+	appId: '',
+	projectId: '',
+	storageBucket: '',
+	authDomain: ''
+};
+
 function LinkScreen() {
+	const [config, setConfig] = useState(defaultConfig);
+	const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setConfig(c => ({...c, [e.target.name]: e.target.value}));
+	};
+	const link = () => {
+		
+	};
+
 	return <CenterForm>
 		<FormHeading>API Key</FormHeading>
-		<TextInput />
+		<TextInput value={config.apiKey} onChange={onChange} name="apiKey" />
 		<FormHeading>App ID</FormHeading>
-		<TextInput />
+		<TextInput value={config.appId} onChange={onChange} name="appId" />
 		<FormHeading>Project ID</FormHeading>
-		<TextInput />
+		<TextInput value={config.projectId} onChange={onChange} name="projectId" />
 		<FormHeading>Storage Bucket</FormHeading>
-		<TextInput />
+		<TextInput value={config.storageBucket} onChange={onChange} name="storageBucket" />
 		<FormHeading>Auth Domain</FormHeading>
-		<TextInput />
+		<TextInput value={config.authDomain} onChange={onChange} name="authDomain" />
 		<FlexEnd>
-			<Button>Connect</Button>
+			<Button onClick={link}>Connect</Button>
 		</FlexEnd>
 	</CenterForm>
 }
